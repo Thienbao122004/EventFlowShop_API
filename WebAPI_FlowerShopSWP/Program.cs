627b361812090b04fb663552e57181fe106173eb
Thienbao122004 <thienbao122004@gmail.com>
2024-09-26 19:06:45 +0700

    Update Progam.cs file

--- WebAPI_FlowerShopSWP/Program.cs
+++ WebAPI_FlowerShopSWP/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebAPI_FlowerShopSWP.Models;
 
 namespace WebAPI_FlowerShopSWP
@@ -9,11 +12,12 @@ namespace WebAPI_FlowerShopSWP
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var secretKey = builder.Configuration["Jwt:SecretKey"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigin",
-                    builder => builder.WithOrigins("http://localhost:5173") // Địa chỉ nguồn của ứng dụng React
+                    builder => builder.WithOrigins("http://localhost:5173")
                                       .AllowAnyMethod()
                                       .AllowAnyHeader());
             });
@@ -25,9 +29,24 @@ namespace WebAPI_FlowerShopSWP
             builder.Services.AddDbContext<FlowerEventShopsContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectDB")));
 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -37,7 +56,9 @@ namespace WebAPI_FlowerShopSWP
             app.UseHttpsRedirection();
 
             // Enable CORS
+
             app.UseCors("AllowSpecificOrigin"); // Sử dụng chính sách CORS đã định nghĩa
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllers();
